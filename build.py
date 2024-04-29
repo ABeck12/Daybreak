bc60c93384c594d068b76d1dc8070350376066cc
Alden <aldenjbeck1@gmail.com>
2024-04-28 21:42:44 -0400

    Updated build.py

--- build.py
+++ build.py
@@ -1,31 +1,38 @@
-import os, platform, sys, subprocess
+import os
+import platform
+import sys
+
 
 def CleanProject():
     system = platform.system()
     if system == 'Windows':
-        os.system('.\scripts\CleanProject.bat')
+        os.system(r'.\scripts\CleanProject.bat')
+
 
 def GenerateVsCode():
     system = platform.system()
     if system == 'Windows':
-       os.system('.\scripts\GenerateProject-vscode.bat')
-        
+        os.system(r'.\scripts\GenerateProject-vscode.bat')
+
 
 def GenerateVS():
     system = platform.system()
     if system == 'Windows':
-        os.system('.\scripts\GenerateProject-VS.bat')
+        os.system(r'.\scripts\GenerateProject-VS.bat')
+
 
 def Compile(flag):
     match flag:
         case '-r': os.system(r'"C:\Program Files\Microsoft Visual Studio\2022\Community\MSBuild\Current\Bin\MSBuild.exe" C:\dev\Daybreak\Daybreak.sln -p:Configuration=Release -maxcpucount')
         case '-d': os.system(r'"C:\Program Files\Microsoft Visual Studio\2022\Community\MSBuild\Current\Bin\MSBuild.exe" C:\dev\Daybreak\Daybreak.sln -p:Configuration=Debug -maxcpucount')
 
+
 def Run(flag):
     os.chdir("Sandbox")
     match flag:
         case '-d': os.startfile(os.path.abspath("../bin/Debug-windows-x86_64/Sandbox/Sandbox.exe"))
         case '-r': os.startfile(os.path.abspath("../bin/Release-windows-x86_64/Sandbox/Sandbox.exe"))
+
 
 def main():
     if len(sys.argv) == 1:
@@ -39,6 +46,7 @@ def main():
         case 'run': Run(sys.argv[2])
         case _: print('Unknown argument! Arguments are "vs","vscode","clean","compile","run"')
     return
+
 
 if __name__ == '__main__':
     main()
