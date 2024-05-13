83ca725bf6a192f451f10595b18564b0c6e4626c
Alden <aldenjbeck1@gmail.com>
2024-05-12 21:54:30 -0400

    Added support to draw frame buffers

--- Daybreak/src/Platform/OpenGL/OpenGLFrameBuffer.h
+++ Daybreak/src/Platform/OpenGL/OpenGLFrameBuffer.h
@@ -12,6 +12,7 @@ namespace Daybreak
 
 		virtual void Bind() const override;
 		virtual void Unbind() const override;
+		virtual const void BindAttachmentAsTexture(uint32_t attachmentIndex = 0, uint32_t textureSlot = 0) const override;
 
 		virtual const FrameBufferSpecifications& GetSpecification() const override { return m_Specification; }
 		virtual const uint32_t GetAttachmentRendererID(uint32_t index = 0) const override { return m_AttachmentIDs[index]; }
@@ -23,7 +24,7 @@ namespace Daybreak
 	private:
 		FrameBufferSpecifications m_Specification;
 		uint32_t m_RendererID;
-
+		std::vector<uint32_t> m_AttachmentEnumsValues;
 		std::vector<uint32_t> m_AttachmentIDs;
 	};
 }
