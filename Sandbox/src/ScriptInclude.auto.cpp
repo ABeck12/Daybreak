378091ef1786db38690695be3b6a820efb73fe11
Alden <aldenjbeck1@gmail.com>
2024-06-16 16:59:58 -0400

    Small changes for sal to run correctly

---
+++ Sandbox/src/ScriptInclude.auto.cpp
@@ -0,0 +1,7 @@
+#include "ScriptInclude.auto.h"
+
+void Daybreak::Auto::AutoScriptInclude()
+{
+	DB_REGISTER_SCRIPT(ArrowKeyMoveable);
+	DB_REGISTER_SCRIPT(MoveableComponent);
+}
---
+++ Sandbox/src/ScriptInclude.auto.h
@@ -0,0 +1,10 @@
+#pragma once
+#include <Daybreak.h>
+
+#include "scripts\ArrowKeyMoveable.h"
+#include "scripts\MoveableComponent.h"
+
+namespace Daybreak::Auto
+{
+	void AutoScriptInclude();
+}
